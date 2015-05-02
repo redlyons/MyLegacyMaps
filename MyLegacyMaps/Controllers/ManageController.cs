4cc33eeb6b06523f8d3667368423ef26d4d853ad
redlyons <reynolds_rob@yahoo.com>
2015-05-02 11:10:48 -0700

    more refactor of template code

--- MyLegacyMaps/Controllers/ManageController.cs
+++ MyLegacyMaps/Controllers/ManageController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using MyLegacyMaps.Models;
+using MyLegacyMaps.Models.Account;
 using MyLegacyMaps.Managers;
 
 namespace MyLegacyMaps.Controllers
---
+++ MyLegacyMaps/Models/Account/ManageLoginsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+
+namespace MyLegacyMaps.Models.Account
+{
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
