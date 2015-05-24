cf911731e1915d76c29bbfbe9b25be464f6c034c
redlyons <reynolds_rob@yahoo.com>
2015-05-24 14:04:06 -0700

    Add flag modal

--- MyLegacyMaps/scripts/mlm/mapdetails.js
+++ MyLegacyMaps/scripts/mlm/mapdetails.js
@@ -2,7 +2,7 @@
 var redflag = "<div class='makeMeDraggable redflag masterTooltip' title='Name:      Date:   '></div>";
 var blueflag = "<div class='makeMeDraggable blueflag masterTooltip'  title='Name:      Date:   '></div>";
 var greenflag = "<div class='makeMeDraggable greenflag masterTooltip'  title='Name:      Date:   '></div>";
-var customLogo = "<div class='makeMeDraggable customLogo masterTooltip'  style='height:75px; width:550px;'></div>"
+var customLogo = "<div class='makeMeDraggable customLogo masterTooltip' title='Name:      Date:   ' style='height:75px; width:550px;'></div>"
 
 function init() {
     $('.makeMeDraggable').draggable({
@@ -12,10 +12,12 @@ function init() {
     });
 
     $(".makeMeDraggable").click(function () {
-        $.get('/modals/washere.html', function (data) {
+        $.get('../../templates/modals/WasHere.html', function (data) {
             modal.open({ content: data });
         });
     });
+
+   
 }
 
 function handleDragStop(event, ui) {
@@ -42,5 +44,25 @@ $(document).ready(function () {
     $("#btnCustomLogo").click(function () {
         $("#content").append(customLogo);
         init();
-    });      
+    });
+
+    // Tooltip only Text
+    $('.masterTooltip').hover(function () {
+        // Hover over code
+        var title = $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+    }, function () {
+        // Hover out code
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+    }).mousemove(function (e) {
+        var mousex = e.pageX + 20; //Get X coordinates
+        var mousey = e.pageY + 10; //Get Y coordinates
+        $('.tooltip')
+        .css({ top: mousey, left: mousex })
+    });
 });
