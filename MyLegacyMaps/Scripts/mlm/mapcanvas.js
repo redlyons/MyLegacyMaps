77ac2c2e7b94bb8e00dc2209ccb95a9957b17f0d
redlyons <reynolds_rob@yahoo.com>
2015-05-30 13:37:29 -0700

    namespace js and combine into one file

--- MyLegacyMaps/Scripts/mlm/mapcanvas.js
+++ MyLegacyMaps/Scripts/mlm/mapcanvas.js
@@ -1,6 +1,7 @@
 ﻿
-var MLM = (function () {
-
+var MLM = {}
+MLM.MapCanvas = (function () {
+    
     var flgWasHere = "<div class='makeMeDraggable flgWasHere masterTooltip' data-xpos='50' data-ypos='50' title='Name:      Date:   '></div>";
     var flgHereNow = "<div class='makeMeDraggable flgHereNow masterTooltip' data-xpos='50' data-ypos='50' title='Name:      Date:   '></div>";
     var flgPlanToGo = "<div class='makeMeDraggable flgPlanToGo masterTooltip' data-xpos='50' data-ypos='50'  title='Name:      Date:   '></div>";
@@ -12,28 +13,28 @@ var MLM = (function () {
         init: function () {
             $('body').css('background', 'url("/images/maps/' + $('#mapFileName').val() + '") no-repeat');
 
-            MLM.wireUpButtonEvents();
-            MLM.wireUpTooltips();
-            MLM.wireUpFlags();
+            MLM.MapCanvas.wireUpButtonEvents();
+            MLM.MapCanvas.wireUpTooltips();
+            MLM.MapCanvas.wireUpFlags();
         },
 
         wireUpButtonEvents: function () {
             $("#btnWasHere").click(function () {
                 $(flgWasHere).attr("id", "flag" + flagId++).appendTo("#content");
                 //$("#content").append(flgWasHere.attr);
-                MLM.wireUpFlags();
+                MLM.MapCanvas.wireUpFlags();
             });
             $("#btnHereNow").click(function () {
                 $("#content").append(flgHereNow);
-                MLM.wireUpFlags();
+                MLM.MapCanvas.wireUpFlags();
             });
             $("#btnWantToGoHere").click(function () {
                 $("#content").append(flgPlanToGo);
-                MLM.wireUpFlags();
+                MLM.MapCanvas.wireUpFlags();
             });
             $("#btnCustomLogo").click(function () {
                 $("#content").append(flgCustomLogo);
-                MLM.wireUpFlags();
+                MLM.MapCanvas.wireUpFlags();
             });
 
         },
@@ -66,7 +67,7 @@ var MLM = (function () {
             $('.makeMeDraggable').draggable({
                 cursor: 'move',
                 snap: '#content',
-                stop: MLM.handleDragStop
+                stop: MLM.MapCanvas.handleDragStop
             });
 
             $(".makeMeDraggable").click(function () {
@@ -79,7 +80,7 @@ var MLM = (function () {
                 else return;
 
                 $.get('../../templates/modals/' + template, function (data) {
-                    modal.open({ content: data, element: flag });
+                    MLM.Modal.open({ content: data, element: flag });
                 });
             });
 
@@ -95,4 +96,74 @@ var MLM = (function () {
         },
     };
 })();
-MLM.init();
+MLM.MapCanvas.init();
+
+
+MLM.Modal = (function () {
+    var
+    method = {},
+    $overlay,
+    $modal,
+    $content,
+    $close;
+
+    // Center the modal in the viewport
+    method.center = function () {
+        var top, left;
+
+        top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+        left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+        $modal.css({
+            top: top + $(window).scrollTop(),
+            left: left + $(window).scrollLeft()
+        });
+    };
+
+    // Open the modal
+    method.open = function (settings) {
+        $content.empty().append(settings.content);
+
+        //stash flag coordinates for use on post
+        $modal.attr('data-xpos', $(settings.element).attr('data-xpos'));
+        $modal.attr('data-ypos', $(settings.element).attr('data-ypos'));
+        $modal.css({
+            width: settings.width || 'auto',
+            height: settings.height || 'auto'
+        });
+
+        method.center();
+        $(window).bind('resize.modal', method.center);
+        $modal.show();
+        $overlay.show();
+    };
+
+    // Close the modal
+    method.close = function () {
+        $modal.hide();
+        $overlay.hide();
+        $content.empty();
+        $(window).unbind('resize.modal');
+    };
+
+    // Generate the HTML and add it to the document
+    $overlay = $('<div id="overlay"></div>');
+    $modal = $('<div id="modal"></div>');
+    $content = $('<div id="modalContent"></div>');
+    $close = $('<a id="close" href="#">close</a>');
+
+    $modal.hide();
+    $overlay.hide();
+    $modal.append($content, $close);
+
+    $(document).ready(function () {
+        $('body').append($overlay, $modal);
+    });
+
+    $close.click(function (e) {
+        e.preventDefault();
+        method.close();
+    });
+
+    return method;
+}());
