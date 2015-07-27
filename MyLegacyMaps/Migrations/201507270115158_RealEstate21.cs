da666069bfff76faefb3bd77163c19d4381cf912
redlyons <reynolds_rob@yahoo.com>
2015-07-26 19:33:45 -0700

    update migration partnerLogoId null

--- MyLegacyMaps/Migrations/201507270115158_RealEstate21.cs
+++ MyLegacyMaps/Migrations/201507270115158_RealEstate21.cs
@@ -8,7 +8,7 @@ namespace MyLegacyMaps.Migrations
         public override void Up()
         {
             DropIndex("dbo.Flags", new[] { "PartnerLogoId" });
-            AlterColumn("dbo.Flags", "PartnerLogoId", c => c.Int(nullable: false));
+            AlterColumn("dbo.Flags", "PartnerLogoId", c => c.Int(nullable: true));
             CreateIndex("dbo.Flags", "PartnerLogoId");
         }
         
